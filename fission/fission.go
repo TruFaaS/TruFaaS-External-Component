d68ef3bc5982640cfc7a45847f1d06292cd73c5d
binoy <binoy.18@cse.mrt.ac.lk>
2023-04-27 11:02:28 +0530

    testing header

--- fission/fission.go
+++ fission/fission.go
@@ -72,7 +72,11 @@ func VerifyFnTrustValue(respWriter http.ResponseWriter, req *http.Request) {
 
 	// check and set client public key value
 	clientPubKeyHeader := req.Header.Get(constants.InvokerPublicKeyHeader)
-	fmt.Println("client PubKey - ", clientPubKeyHeader)
+	for name, values := range req.Header {
+		for _, value := range values {
+			fmt.Printf("%s: %s\n", name, value)
+		}
+	}
 
 	// get the json value and convert to struct
 	err := json.NewDecoder(req.Body).Decode(&function)
